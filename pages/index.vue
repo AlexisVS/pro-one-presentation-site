1a1cc65dc0e29ae37c428bdd639c92f5840ef897
AlexisVS <alexis.vansan1440@gmail.com>
2023-08-07 04:26:17 +0200

    try to success deploy on github pages

--- pages/index.vue
+++ pages/index.vue
@@ -7,16 +7,46 @@ import TransitionBase from "../components/Transitions/TransitionBase.vue";
     <HomePageSocietyWhoWeAre />
     <TransitionBase>
       <p>Lorem ipsum dolor sit amet</p>
-      <img src="@/assets/images/brand/brand.png" alt="" />
-      <img src="~/assets/images/brand/brand.png" alt="" />
-      <img src="assets/images/brand/brand.png" alt="" />
-      <img src="/images/brand/brand.png" alt="" />
+      <!--      <img src="@/assets/images/brand/brand.png" alt="" />-->
+      <!--      <img src="~/assets/images/brand/brand.png" alt="" />-->
+      <!--      <img src="assets/images/brand/brand.png" alt="" />-->
       <img src="public/images/brand/brand.png" alt="" />
       <img src="/public/images/brand/brand.png" alt="" />
       <img src="/images/brand/brand.png" alt="" />
-      <img src="images/brand/brand.png" alt="" />
-      <img src="/brand/brand.png" alt="" />
-      <img src="brand/brand.png" alt="" />
+
+      <NuxtImg
+        class="logo"
+        src="./images/brand/brand.png"
+        alt="Logo"
+        height="40"
+      />
+      <NuxtImg
+        class="logo"
+        src="/public/images/brand/brand.png"
+        alt="Logo"
+        height="40"
+      />
+      <NuxtImg
+        class="logo"
+        src="public/images/brand/brand.png"
+        alt="Logo"
+        height="40"
+      />
+      <NuxtImg
+        class="logo"
+        src="/images/brand/brand.png"
+        alt="Logo"
+        height="40"
+      />
+      <NuxtImg
+        class="logo"
+        src="images/brand/brand.png"
+        alt="Logo"
+        height="40"
+      />
+      <!--      <img src="images/brand/brand.png" alt="" />-->
+      <!--      <img src="/brand/brand.png" alt="" />-->
+      <!--      <img src="brand/brand.png" alt="" />-->
     </TransitionBase>
   </div>
 </template>
