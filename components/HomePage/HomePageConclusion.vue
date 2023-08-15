f88662042d04e35faa96736cadcd67e897e4de34
AlexisVS <alexis.vansan1440@gmail.com>
2023-08-15 09:55:21 +0200

    update

--- components/HomePage/HomePageConclusion.vue
+++ components/HomePage/HomePageConclusion.vue
@@ -7,6 +7,10 @@
         <h2 class="fluid-font-size-3">{{ $t("home.conclusion.title") }}</h2>
         <p class="fluid-font-size-0">{{ $t("home.conclusion.paragraph_1") }}</p>
         <p class="fluid-font-size-0">{{ $t("home.conclusion.paragraph_2") }}</p>
+        <div class="conclusion__text-side__cta">
+          <ButtonPage text="Nos appareils à pression" to="/pression/" />
+          <ButtonPage text="Nos appareils à gravité" to="/gravity/" />
+        </div>
       </div>
       <div class="conclusion__image-side">
         <NuxtImg
@@ -16,14 +20,21 @@
         />
       </div>
     </div>
-    <ButtonPage class="btn" text="Nos appareils à pression" to="/pression/" />
-    <ButtonPage class="btn" text="Nos appareils à gravité" to="/gravity/" />
   </section>
 </template>
 
 <style lang="scss" scoped>
 .conclusion {
   padding: 0 1rem;
+
+  &__text-side {
+    &__cta {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      margin-top: 1rem;
+    }
+  }
 
   &__image-side {
     width: 100%;
@@ -42,7 +53,20 @@
 
     &__text-side {
       padding-left: 1rem;
+      justify-content: flex-start;
       width: 50%;
+
+      &__cta {
+        display: flex;
+        margin-top: 1rem;
+        flex-direction: row;
+        flex-wrap: wrap;
+
+        & > *:first-child {
+          margin-right: 1rem;
+          margin-bottom: 1rem;
+        }
+      }
     }
   }
 }
--- modules/i18n/i18n.config.ts
+++ modules/i18n/i18n.config.ts
@@ -4,7 +4,6 @@ import fr_FR from "./lang/fr-FR.json";
 
 export default defineI18nConfig(() => ({
   legacy: false,
-  strategy: "prefix_except_default",
   fallbackLocale: process.env.NUXT_PUBLIC_LANGUAGE || "fr-FR",
   defaultLocale: process.env.NUXT_PUBLIC_LANGUAGE || "fr-FR",
   messages: {
