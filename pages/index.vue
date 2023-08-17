1e18db3f514343033507c32be598796567aac298
AlexisVS <alexis.vansan1440@gmail.com>
2023-08-17 18:56:16 +0200

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,4 @@
-<script lang="ts" setup>
-const navigation = ref<Element | null>(null);
-
-const handleScroll = (navigation: Element): any => {
-  if (navigation) {
-    if (window.scrollY <= 65) {
-      navigation.classList.add("top");
-    } else {
-      navigation.classList.remove("top");
-    }
-  }
-};
-
-onMounted(() => {
-  navigation.value = document.querySelector("#navigation");
-  if (navigation.value) {
-    const navigationElement = navigation.value as Element;
-    window.addEventListener("scroll", handleScroll(navigationElement));
-  }
-});
-
-onUnmounted(() => {
-  const navigationElement = navigation.value as Element;
-  window.removeEventListener("scroll", handleScroll(navigationElement));
-});
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <div>
@@ -34,16 +9,3 @@ onUnmounted(() => {
     <PageHomeConclusion />
   </div>
 </template>
-
-<style lang="scss">
-@use "sass:color";
-@import "~/assets/scss/_variables.scss";
-
-#navigation.top {
-  /* Add your styles for the navigation when at the top */
-  background: linear-gradient(
-    rgba(0, 0, 0, 0.4) 60%,
-    rgba(0, 0, 0, 0)
-  ) !important;
-}
-</style>
